9d8813765a464fa39dd5f122e8edfd93a2b491a1
Recillah-Khamala <recillahk@gmail.com>
2023-06-15 17:19:15 -0700

    Remove white spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,13 @@
 /**
  * main - Entry point of the program
  *
- * Description: This progrsm prints the message 
+ * Description: This progrsm prints the message
  * "\"Programming is like building a multilingual puzzle"
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
-
-int main(void) 
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
