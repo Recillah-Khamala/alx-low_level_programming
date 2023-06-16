b76a1603c1b871a87aeba9851d41eb618650e315
Recillah-Khamala <recillahk@gmail.com>
2023-06-15 18:10:37 -0700

    Modify program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,18 +12,16 @@ int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a short int: %lu byte(s)\n", sizeof(short int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	printf("Size of a double: %lu byte(s)\n", sizeof(double));
 	printf("Size of a long double: %lu byte(s)\n", sizeof(long double));
-	printf("Size of void: %lu byte(s)\n", sizeof(void));
-	printf("Size of a pointer: %lu byte(s)\n", sizeof(void *));
-	printf("Size of a bool: %lu byte(s)\n", sizeof(_Bool));
-	printf("Size of an enum: %lu byte(s)\n", sizeof(enum));
-	printf("Size of a struct: %lu byte(s)\n", sizeof(struct));
-	printf("Size of a union: %lu byte(s)\n", sizeof(union));
+	printf("Size of a void pointer: %lu byte(s)\n", sizeof(void *));
+	printf("Size of a _Bool: %lu byte(s)\n", sizeof(_Bool));
+	printf("Size of an enum: %lu byte(s)\n", sizeof(enum {VALUE}));
+	printf("Size of a struct: %lu byte(s)\n", sizeof(struct {int member;}));
+	printf("Size of a union: %lu byte(s)\n", sizeof(union {int member;}));
 
 	return (0);
 }
