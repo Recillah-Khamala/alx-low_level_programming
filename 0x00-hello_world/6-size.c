31da1052b1545a6a8af67ae57b6466747faedc79
Recillah-Khamala <recillahk@gmail.com>
2023-06-15 18:18:47 -0700

    Reduce program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main -Entry point of the program
  *
- * Description: This program prints the size of types on the 
+ * Description: This program prints the size of types on the
  * computer it is compiles and run on.
  *
  * Return: 0 (success)
@@ -15,13 +15,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-	printf("Size of a double: %lu byte(s)\n", sizeof(double));
-	printf("Size of a long double: %lu byte(s)\n", sizeof(long double));
-	printf("Size of a void pointer: %lu byte(s)\n", sizeof(void *));
-	printf("Size of a _Bool: %lu byte(s)\n", sizeof(_Bool));
-	printf("Size of an enum: %lu byte(s)\n", sizeof(enum {VALUE}));
-	printf("Size of a struct: %lu byte(s)\n", sizeof(struct {int member;}));
-	printf("Size of a union: %lu byte(s)\n", sizeof(union {int member;}));
 
 	return (0);
 }
