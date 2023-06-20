005b6a7d63d9d1f1f8e4746092ea8ca5d2f8f19a
Recillah-Khamala <recillahk@gmail.com>
2023-06-20 08:36:07 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints "_putchar" followed by a new line.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char text[] = "_putchar\n";
+	int i = 0;
+
+	while (text[i] != '\0')
+		i++;
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file: main.h
+ *
+ * Description: This file contains prototypes of all functions.
+ */
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
