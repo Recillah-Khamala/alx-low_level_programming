bd6cf4decb0167d6e7a45f2da3668c13820861c4
Recillah-Khamala <recillahk@gmail.com>
2023-06-21 00:11:51 -0700

    Add a _putchar file.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,11 +9,15 @@
  */
 int main(void)
 {
-	char text[] = "_putchar\n";
-	int i = 0;
+	char text[] = "_putchar";
+	int i;
 
-	while (text[i] != '\0')
-		i++;
+	for (i=0; i<8; i++)
+	{
+		_putchar(text[i]);
+	}
+
+	_putchar('\n');
 
 	return (0);
 }
